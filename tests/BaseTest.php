5b49f81c1c9d4d0d8b5ae25a64539cb8459d1079
lzy <fuzzyy@foxmail.com>
2020-03-24 12:04:27 +0800

    增加单元测试

---
+++ tests/BaseTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Test\AlibabaOpen;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class BaseTest 测试基类
+ * @package Test\CQApi
+ */
+class BaseTest extends TestCase
+{
+}
---
+++ tests/OrderTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\AlibabaOpen;
+
+/**
+ * Class OrderTest
+ * @internal
+ */
+class OrderTest extends BaseTest
+{
+    protected $coupon;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * 商户购买卡劵接口
+     */
+    public function testIndex()
+    {
+    }
+}
