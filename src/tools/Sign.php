abc90d100c5ba0c400fb8106906f197115247817
lzy <fuzzyy@foxmail.com>
2020-04-14 17:05:46 +0800

    优化验证消息签名机制

--- src/tools/Sign.php
+++ src/tools/Sign.php
@@ -7,7 +7,7 @@ final class Sign
     /**
      * 验证消息签名
      *
-     * @param $params
+     * @param array $params 一定要包含message和_aop_signature
      * @param $appSecret
      * @return bool
      * @see https://open.1688.com/doc/msgOverview.htm?id=546049
@@ -15,9 +15,10 @@ final class Sign
     public static function verifySign(array $params, $appSecret)
     {
         if (empty($params['message']) || empty($params['_aop_signature'])) {
+            //缺少关键参数
             return false;
         }
-        $sign = hash_hmac('sha1', 'message' . $params['message'], $appSecret);
+        $sign = hash_hmac('sha1', 'message' . html_entity_decode($params['message']), $appSecret);//多次解码不会出现问题
         return strtoupper($sign) == $params['_aop_signature'];
     }
 
